5d7f62a68d7b750c1d87d1d24c4b83ad3d04b782
jiduslove <oondayr@gamil.com>
2023-03-30 10:38:02 +0900

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ function App() {
     <div>
       <h1>Hello, React!</h1>
       <h2>My Name is Dio</h2>
-      <h3>My Favorite drink is Millkiss</h3>
+      <h3>My Favorite drink is Millkiss 👄</h3>
     </div>
   );
 }
