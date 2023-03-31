8171f9fb3a3e14340ce43973abc1cb375a1c4f07
jiduslove <oondayr@gamil.com>
2023-03-31 15:16:48 +0900

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,82 +1,19 @@
-// import Adidas from "./images/adidas.png";
+import Header from "./components/Header";
+import Introduce from "./components/Introduce";
+import Portfolio from "./components/Portfolio";
+import ContactMe from "./components/ContactMe";
 
 function App() {
   return (
     <div className="bg-red-100 min-h-screen">
-      <header className="bg-blue-100 h-12 md:h-20">
-        <div className="max-w-screen-xl mx-auto h-full flex justify-between items-center">
-          <div>Dio's Portfolio</div>
-          {/* <div>
-            <img className="w-12" src={Adidas} alt="Adidas" />
-          </div> */}
-          <ul className="flex gap-2 md:gap-8 text-xs sm:text-sm md:text-base">
-            <li className="bg-indigo-300 btn-style">Introduce</li>
-            <li className="bg-orange-300 btn-style">Portfolio</li>
-            <li className="bg-blue-300 btn-style">Contact Me</li>
-          </ul>
-        </div>
-      </header>
+      <Header />
       <main>
-        {/* Introduce */}
-        <div className="bg-green-100 max-w-screen-xl mx-auto h-96 flex justify-center items-center">
-          저는 파란남방입은 남자 입니다.
-        </div>
-        <ul className="bg-indigo-100 max-w-screen-xl mx-auto grid grid-cols-1 grid grid-cols-2 grid grid-cols-3 grid grid-cols-4 justify-items-center gap-8 py-8">
-          <li className="bg-purple-300 w-60 h-72 rounded-xl">
-            {/* object-fit: cover */}
-            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
-              프로젝트 이미지
-            </div>
-            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
-            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
-          </li>
-          <li className="bg-purple-300 w-60 h-72 rounded-xl">
-            {/* object-fit: cover */}
-            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
-              프로젝트 이미지
-            </div>
-            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
-            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
-          </li>
-          <li className="bg-purple-300 w-60 h-72 rounded-xl">
-            {/* object-fit: cover */}
-            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
-              프로젝트 이미지
-            </div>
-            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
-            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
-          </li>
-          <li className="bg-purple-300 w-60 h-72 rounded-xl">
-            {/* object-fit: cover */}
-            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
-              프로젝트 이미지
-            </div>
-            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
-            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
-          </li>
-          <li className="bg-purple-300 w-60 h-72 rounded-xl">
-            {/* object-fit: cover */}
-            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
-              프로젝트 이미지
-            </div>
-            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
-            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
-          </li>
-          <li className="bg-purple-300 w-60 h-72 rounded-xl">
-            {/* object-fit: cover */}
-            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
-              프로젝트 이미지
-            </div>
-            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
-            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
-          </li>
-        </ul>
-        <div className="bg-gray-100 max-w-screen-xl mx-auto h-96 flex justify-center items-center">
-          내 연락처
-        </div>
+        <Introduce />
+        <Portfolio />
+        <ContactMe />
       </main>
-      <footer className="max-w-screen-xl mx-auto flex justify-end py-1 md:py-2">
-        2023,Designed by, Dio
+      <footer className="max-w-screen-xl mx-auto flex justify-end items-center py-1 md:py-2">
+        2023, Designed by, Dio
       </footer>
     </div>
   );
